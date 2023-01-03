03c31eb8fd008c9cbcd325cc1b451a607d0e2d4c
Greg Jones <greg@lg2solutions.com>
2023-01-03 15:54:59 -0500

    This version is working in Google and Local VSCode

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Speech-To-Text-Google
 
-Running the Speech to Text Google samples
+Running the Speech to Text Google samples - codelabs
+<https://codelabs.developers.google.com/codelabs/cloud-speech-text-python3/#0>
+
+Also asked ChatGPT how to do it locally with this question:
+"How do I setup my windows vscode environment to support a python project that uses google service account"
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import io
+import os
+
 from google.cloud import speech_v1 as speech
+
+#setting Google credentials
+os.environ['GOOGLE_APPLICATION_CREDENTIALS']='C:\Dev\LG2 Solutions\Speech-To-Text-Google\gcloud_do_not_share\dictation-sample-82bc323451d9.json'
 
 
 def speech_to_text(config, audio):
@@ -15,7 +21,18 @@ def print_sentences(response):
         print("-" * 80)
         print(f"Transcript: {transcript}")
         print(f"Confidence: {confidence:.0%}")
+        
+
+#config = dict(language_code="en-US")
+#audio = dict(uri="gs://cloud-samples-data/speech/brooklyn_bridge.flac")
+config = speech.RecognitionConfig(
+    encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+    enable_automatic_punctuation=True,
+    audio_channel_count=2,
+    language_code="en-US"
+)
+audio = dict(uri="https://www.voiptroubleshooter.com/open_speech/american/OSR_us_000_0010_8k.wav")
+
+speech_to_text(config,audio)
 
 
-config = dict(language_code="en-US")
-audio = dict(uri="gs://cloud-samples-data/speech/brooklyn_bridge.flac")
